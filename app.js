b2de556fb59536bc469b72f89f27db81a6390627
OEM Configuration (temporary user) <erivadeneira@uni.pe>
2024-02-07 15:19:04 -0500

    My first commit

---
+++ app.js
@@ -0,0 +1,56 @@
+const API_KEY="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"
+const submitButton = document.querySelector('#submit')
+const outPutElement = document.querySelector('#output')
+const inputElement = document.querySelector('input')
+const historyElement = document.querySelector('.history')
+const buttonElement = document.querySelector('button')
+
+function changeInput(value){
+    const inputElement = document.querySelector('input')
+    inputElement.value = value
+}
+
+async function getMessage(){
+    console.log('clicked')
+    options = {
+        method: 'POST',
+        headers:{
+            'Authorization': `Bearer ${API_KEY}`,
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            model:"gpt-3.5-turbo",
+            messages: [{
+                role:"user",
+                content:inputElement.value
+            }],
+            max_tokens:100
+        })
+    }
+    try{
+           const response = await fetch('https://api.openai.com/v1/chat/completions', options)
+           const data = await response.json()
+           console.log(data)
+           outPutElement.textContent = data.choices[0].message.content
+
+
+            if(data.choices[0].message.content && inputElement.value){
+                const pElement = document.createElement('p')
+                pElement.textContent = inputElement.value
+                pElement.addEventListener('click', () => changeInput(pElement.textContent))
+
+                historyElement.append(pElement)
+            }
+
+
+    }catch(error){ 
+            console.log(error)
+    }
+}
+
+function clearInput(){
+    inputElement.value = ' '
+}
+
+submitButton.addEventListener('click', getMessage)
+buttonElement.addEventListener('click', clearInput)
